839f36f7ee79a24f7d4601e131b3c6861138f737
tonpm14 <uo266575@uniovi.es>
2023-06-08 11:11:17 +0200

    Initial commit

---
+++ src/main/java/com/search/preflight_assignment/PreflightAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.search.preflight_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreflightAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreflightAssignmentApplication.class, args);
+    }
+
+}
